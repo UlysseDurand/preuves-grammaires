0e6d56605c4df3b3d8ba0994288ad5866a09f294
Ulysse Durand <ulyssedurand@hotmail.fr>
2021-12-25 17:43:25 +0100

    C'est du propre

--- atest.ml
+++ atest.ml
@@ -7,3 +7,15 @@ let rec n_cartesian_product = function
 
 
 n_cartesian_product [[4;7] ; [1;0] ; [3;8]];;
+
+let parcours_spe a = 
+  let rec aux foret dejavu =
+    if toutfeuille foret then listoutfeuille foret else
+      let laforet = if finhauteur foret dejavu then
+        List.flatten (List.map (fun x -> match x with |B(_) -> [x] |N(r,l) -> l))
+      else foret in
+      let a,b,c = trouve laforet dejavu in
+      let filsb = match b with |B(_) -> failwith "erreur" | N(r,l) -> l in
+      let filsdansa = List.flatten (List.map (fun x -> match x with |B(_) -> [x] |N(r,l)->l) a ) in
+      ((lisracines filsdansa)@(lisracines filsb)@c)::(aux foret (b::dejavu))
+  aux [] [];;
