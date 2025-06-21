1de8ef8311a350f0f8448d9336151e1b1c282c31
Ulysse Durand <ulysse.durand@ens-lyon.fr>
2025-06-21 19:24:46 +0530

    working now

--- deployment/ocamlrun/entrypoint.sh
+++ deployment/ocamlrun/entrypoint.sh
@@ -5,10 +5,11 @@ cp /tmp/input.txt /tmp/main.ml
 
 # Contruction of the output.txt file
 cd /tmp
-cat typesetutiles.ml > tmp.ml
-cat preuveautomatique.ml >> tmp.ml
-cat simplifie.ml >> tmp.ml
-cat main.ml >> tmp.ml
-opam exec ocaml < '#use "tmp.ml";;' > output.txt
+cat typesetutiles.ml > tout.ml
+cat preuveautomatique.ml >> tout.ml
+cat simplifie.ml >> tout.ml
+cat main.ml >> tout.ml
+echo "#use \"tout.ml\";;" > tmp.ml
+opam exec ocaml < tmp.ml > output.txt
 
 cp output.txt $WORK_DIR/output.txt
